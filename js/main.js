adb7c8661246848d792b913c5cb48ccba665818b
JohnDoes <35497595+giagabo@users.noreply.github.com>
2019-03-10 17:18:53 +0400

    Cold Start

---
+++ js/main.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// var cvladi = 'cvladi' // გამოძახება ხდება ნებისმიერ ადგილას და აქვს გლობალური წვდომა
+// let cVladi = 1; // მუშაობს მხოლოდ მაშინ როდესაც კოდი მიდის ცვლადამე და უკვე შემდეგ შეგვიძლია ამ ცვლადთან თამაში.
+// const cvLadi = 2; არ იცვლება, მაგრამ არის მომენტი სადაც გვეძ₾ევა საშუალება მისი შეცვლის
+
+
+//მონაცემთა ტიპი
+// var sym = symbol(); //
+// null;//
+// undefined;//
+// let obj = {
+//     name: "Ga",
+//     age: 30,
+//     ismArried: false
+// }; //
+// if (obj.ismArried){
+//     console.log('დაქორწინებულია')
+// }else{
+//     console.log('ucolo')
+// }
+// console.log(obj.name + ' ' + obj.age); // გამოგვაქს კონსოლში ქეი ნეიმში არსებულისთრინგი. თუ ქეიში არსებული სთრინგი/მონაცემები საკმაოდ გრძელია მშინ კონსოლში გამოგვაქვს
+//ასე
+// console.log(obj["name"]);
+
+// //Masiveni
+// let masivi = ['plum.pmg', 'Orange.jpg', 'apple.bmp'];
+// console.log(masivi[2]);
+// let answer = confirm('aq xar?');
+
+// let answer = prompt('radenis xar?')
+
+// console.log(typeof(answer)); // typeof -ის შემთხვევაში კონსოლში გვექნება ინფო თუ რა ტიპის მონაცემებთან გვაქვს საქმე. სთრინგი, ობიექტი ასე შემდეგ ასე შემდეგ 
+
+//ოპერატორები == ertia === adarebs tipebs != tu araris tolie !===
